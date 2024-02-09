4e6a3b64c7094aeef121554115de1fc13c9d8906
sheninthjr <sheninthjr23@gmail.com>
2024-02-09 14:07:45 +0530

    Animation

--- app/components/Landing.tsx
+++ app/components/Landing.tsx
@@ -5,25 +5,49 @@ import About from "./About";
 export const Landing = () => {
   return (
     <>
-      <div className="h-screen bg-cover w-full bg-[url('/Landing.jpg')]">
-        <div className="flex flex-col justify-center items-center text-white h-screen">
+      <div className="h-screen bg-cover w-full bg-[url('/Landing.jpg')] overflow-x-hidden">
+        <div className="flex flex-col justify-center items-center text-white h-screen gap-10">
           <div className="font-extrabold text-6xl md:text-8xl font-serif">
             ICDAIC&lsquo;24
           </div>
-          <div className="text-2xl md:text-3xl font-serif text-center">
+          <div className="text-2xl font-bold md:text-3xl font-serif text-center">
             International Level Conference
           </div>
-          <div className="flex space-x-10 md:flex-row md:space-x-32 lg:space-x-36 pt-12 text-lg md:text-3xl font-serif">
-            <div className="text-yellow-400">
+          <div className="flex space-x-10 md:flex-row md:space-x-32 lg:space-x-36 pt-12 text-2xl font-extrabold md:text-3xl lg:text-6xl font-serif">
+            <div className="text-yellow-400 animate-pulse">
               <a href="/">Register now</a>
             </div>
             <div>
               <a href="/">View Details</a>
             </div>
           </div>
+          <div
+            style={{ whiteSpace: "nowrap" }}
+            className="animate-text text-yellow-400 text-xl font-bold w-full"
+          >
+            <a href="" target="_blank">
+              For Scopus/SCI indexed Click Here
+            </a>
+          </div>
         </div>
         <About />
       </div>
+      <style>
+        {`
+          @keyframes moveRightToLeft {
+            from {
+              transform: translateX(100%);
+            }
+            to {
+              transform: translateX(-100%);
+            }
+          }
+          
+          .animate-text {
+            animation: moveRightToLeft 15s linear infinite; 
+          }
+        `}
+      </style>
     </>
   );
 };
